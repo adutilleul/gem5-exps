1e56996bac26022817ad42122a5eea9665cc7150
Alban Dutilleul <alban.dutilleul@ens-rennes.fr>
2024-10-07 20:49:56 +0200

    add Golden Cove + extract mapping from uops.info

--- processors/goldencove.py
+++ processors/goldencove.py
@@ -31,100 +31,132 @@ import m5
 from m5.objects import *
 import math
 
-class SKXPort0(FUDesc):
+class ADLPPort00(FUDesc):
     opList = [
-        OpDesc(opClass="FloatCmp", opLat=4, pipelined=True),
-        OpDesc(opClass="FloatAdd", opLat=4, pipelined=True),
-        OpDesc(opClass="FloatSqrt", opLat=12, pipelined=False),
         OpDesc(opClass="FloatDiv", opLat=11, pipelined=False),
-        OpDesc(opClass="SimdFloatDiv", opLat=11, pipelined=False),
-        OpDesc(opClass="SimdFloatAdd", opLat=4, pipelined=True),
         OpDesc(opClass="IntMult", opLat=4, pipelined=True),
-        OpDesc(opClass="SimAdd", opLat=1, pipelined=True),
-        OpDesc(opClass="SimdFloatSqrt", opLat=12, pipelined=False),
-        OpDesc(opClass="FloatCvt", opLat=6, pipelined=True),
-        OpDesc(opClass="SimdCmp", opLat=1, pipelined=True),
-        OpDesc(opClass="SimdMult", opLat=5, pipelined=True),
-        OpDesc(opClass="SimdShift", opLat=2, pipelined=True),
-        OpDesc(opClass="IntDiv", opLat=76, pipelined=False),
-        OpDesc(opClass="SimdAlu", opLat=1, pipelined=True),
-        OpDesc(opClass="FloatMult", opLat=4, pipelined=True),
         OpDesc(opClass="SimdFloatMult", opLat=4, pipelined=True),
-        OpDesc(opClass="SimdFloatCmp", opLat=4, pipelined=True)
-    ]
-    count = 1
-
-class SKXPort1(FUDesc):
-    opList = [
+        OpDesc(opClass="FloatSqrt", opLat=12, pipelined=False),
+        OpDesc(opClass="SimdMult", opLat=5, pipelined=True),
         OpDesc(opClass="FloatCmp", opLat=4, pipelined=True),
-        OpDesc(opClass="FloatAdd", opLat=4, pipelined=True),
+        OpDesc(opClass="IntAlu", opLat=1, pipelined=True),
         OpDesc(opClass="SimAdd", opLat=1, pipelined=True),
-        OpDesc(opClass="IntMult", opLat=4, pipelined=True),
-        OpDesc(opClass="SimdCmp", opLat=1, pipelined=True),
-        OpDesc(opClass="SimdFloatAdd", opLat=4, pipelined=True),
-        OpDesc(opClass="FloatCvt", opLat=6, pipelined=True),
-        OpDesc(opClass="SimdMult", opLat=5, pipelined=True),
         OpDesc(opClass="SimdShift", opLat=2, pipelined=True),
-        OpDesc(opClass="IntDiv", opLat=76, pipelined=False),
+        OpDesc(opClass="SimdCmp", opLat=1, pipelined=True),
+        OpDesc(opClass="SimdFloatDiv", opLat=11, pipelined=False),
         OpDesc(opClass="SimdAlu", opLat=1, pipelined=True),
+        OpDesc(opClass="SimdFloatCvt", opLat=4, pipelined=True),
+        OpDesc(opClass="SimdFloatCmp", opLat=4, pipelined=True),
+        OpDesc(opClass="IntDiv", opLat=15, pipelined=False),
+        OpDesc(opClass="SimdFloatSqrt", opLat=12, pipelined=False),
         OpDesc(opClass="FloatMult", opLat=4, pipelined=True),
-        OpDesc(opClass="SimdFloatMult", opLat=4, pipelined=True),
-        OpDesc(opClass="SimdFloatCmp", opLat=4, pipelined=True)
+        OpDesc(opClass="FloatCvt", opLat=7, pipelined=True)
     ]
     count = 1
-
-class SKXPort2(FUDesc):
-    opList = [
-        OpDesc(opClass="FloatMemWrite", opLat=1, pipelined=True),
-        OpDesc(opClass="FloatMemRead", opLat=5, pipelined=True),
-        OpDesc(opClass="MemWrite", opLat=1, pipelined=True),
-        OpDesc(opClass="MemRead", opLat=5, pipelined=True)
-    ]
-    count = 1
-
-class SKXPort3(FUDesc):
-    opList = [
-        OpDesc(opClass="FloatMemWrite", opLat=1, pipelined=True),
-        OpDesc(opClass="FloatMemRead", opLat=5, pipelined=True),
-        OpDesc(opClass="MemWrite", opLat=1, pipelined=True),
-        OpDesc(opClass="MemRead", opLat=5, pipelined=True)
-    ]
-    count = 1
-
-class SKXPort4(FUDesc):
-    opList = [
-        OpDesc(opClass="FloatMemWrite", opLat=1, pipelined=True),
-        OpDesc(opClass="MemWrite", opLat=1, pipelined=True)
-    ]
-    count = 1
-
-class SKXPort5(FUDesc):
+    
+class ADLPPort01(FUDesc):
     opList = [
         OpDesc(opClass="SimdMisc", opLat=1, pipelined=True),
         OpDesc(opClass="IntMult", opLat=4, pipelined=True),
-        OpDesc(opClass="SimdCmp", opLat=1, pipelined=True),
+        OpDesc(opClass="SimdFloatMult", opLat=4, pipelined=True),
+        OpDesc(opClass="SimdMult", opLat=5, pipelined=True),
+        OpDesc(opClass="FloatCmp", opLat=4, pipelined=True),
+        OpDesc(opClass="IntAlu", opLat=1, pipelined=True),
+        OpDesc(opClass="SimAdd", opLat=1, pipelined=True),
         OpDesc(opClass="SimdShift", opLat=2, pipelined=True),
-        OpDesc(opClass="IntDiv", opLat=76, pipelined=False),
-        OpDesc(opClass="SimdAlu", opLat=1, pipelined=True)
+        OpDesc(opClass="SimdFloatCmp", opLat=3, pipelined=True),
+        OpDesc(opClass="SimdCmp", opLat=1, pipelined=True),
+        OpDesc(opClass="SimdFloatAdd", opLat=3, pipelined=True),
+        OpDesc(opClass="SimdAlu", opLat=1, pipelined=True),
+        OpDesc(opClass="SimdFloatCvt", opLat=4, pipelined=True),
+        OpDesc(opClass="SimdFloatCmp", opLat=4, pipelined=True),
+        OpDesc(opClass="IntDiv", opLat=15, pipelined=False),
+        OpDesc(opClass="FloatMult", opLat=4, pipelined=True),
+        OpDesc(opClass="FloatCvt", opLat=7, pipelined=True)
     ]
     count = 1
 
-class SKXPort6(FUDesc):
+class ADLPPort02(FUDesc):
     opList = [
-        OpDesc(opClass="IntDiv", opLat=76, pipelined=False),
-        OpDesc(opClass="IntMult", opLat=4, pipelined=True)
+        OpDesc(opClass="MemRead", opLat=5, pipelined=True),
+        OpDesc(opClass="FloatMemRead", opLat=5, pipelined=True)
     ]
     count = 1
 
-class SKXPort7(FUDesc):
+class ADLPPort03(FUDesc):
     opList = [
-        OpDesc(opClass="FloatMemWrite", opLat=1, pipelined=True),
-        OpDesc(opClass="MemWrite", opLat=1, pipelined=True)
+        OpDesc(opClass="MemRead", opLat=5, pipelined=True),
+        OpDesc(opClass="FloatMemRead", opLat=5, pipelined=True)
+    ]
+    count = 1
+
+class ADLPPort04(FUDesc):
+    opList = [
+        OpDesc(opClass="FloatMemWrite", opLat=12, pipelined=True),
+        OpDesc(opClass="MemWrite", opLat=12, pipelined=True)
+    ]
+    count = 1
+
+class ADLPPort05(FUDesc):
+    opList = [
+        OpDesc(opClass="SimdMisc", opLat=1, pipelined=True),
+        OpDesc(opClass="IntMult", opLat=4, pipelined=True),
+        OpDesc(opClass="IntAlu", opLat=1, pipelined=True),
+        OpDesc(opClass="FloatAdd", opLat=3, pipelined=True),
+        OpDesc(opClass="SimdShift", opLat=2, pipelined=True),
+        OpDesc(opClass="SimdCmp", opLat=1, pipelined=True),
+        OpDesc(opClass="SimdFloatAdd", opLat=3, pipelined=True),
+        OpDesc(opClass="SimdAlu", opLat=1, pipelined=True),
+        OpDesc(opClass="IntDiv", opLat=15, pipelined=False),
+        OpDesc(opClass="SimdFloatCmp", opLat=3, pipelined=True)
+    ]
+    count = 1
+
+class ADLPPort06(FUDesc):
+    opList = [
+        OpDesc(opClass="IntAlu", opLat=1, pipelined=True),
+        OpDesc(opClass="IntMult", opLat=4, pipelined=True),
+        OpDesc(opClass="IntDiv", opLat=15, pipelined=False)
+    ]
+    count = 1
+
+class ADLPPort07(FUDesc):
+    opList = [
+        OpDesc(opClass="FloatMemWrite", opLat=12, pipelined=True),
+        OpDesc(opClass="MemWrite", opLat=12, pipelined=True)
+    ]
+    count = 1
+
+class ADLPPort08(FUDesc):
+    opList = [
+        OpDesc(opClass="FloatMemWrite", opLat=12, pipelined=True),
+        OpDesc(opClass="MemWrite", opLat=12, pipelined=True)
+    ]
+    count = 1
+
+class ADLPPort09(FUDesc):
+    opList = [
+        OpDesc(opClass="FloatMemWrite", opLat=12, pipelined=True),
+        OpDesc(opClass="MemWrite", opLat=12, pipelined=True)
+    ]
+    count = 1
+
+class ADLPPort10(FUDesc):
+    opList = [
+        OpDesc(opClass="IntAlu", opLat=1, pipelined=True),
+        OpDesc(opClass="IntMult", opLat=4, pipelined=True),
+        OpDesc(opClass="IntDiv", opLat=15, pipelined=False)
+    ]
+    count = 1
+class ADLPPort11(FUDesc):
+    opList = [
+        OpDesc(opClass="MemRead", opLat=5, pipelined=True),
+        OpDesc(opClass="FloatMemRead", opLat=5, pipelined=True)
     ]
     count = 1
 
 class ExecUnits(FUPool):
-    FUList = [SKXPort0(), SKXPort1(), SKXPort2(), SKXPort3(), SKXPort4(), SKXPort5(), SKXPort6(), SKXPort7()]
+    FUList = [ADLPPort00(), ADLPPort01(), ADLPPort02(), ADLPPort03(), ADLPPort04(), ADLPPort05(), ADLPPort06(), ADLPPort07(), ADLPPort08(), ADLPPort09(), ADLPPort10(), ADLPPort11()]
 
 class IndirectPred(SimpleIndirectPredictor):
     indirectSets = 128 # Cache sets for indirect predictor
@@ -142,15 +174,18 @@ class LTAGE_BP(LTAGE_TAGE):
     logUResetPeriod = 19
 
 class BranchPred(LTAGE):
-    btb = SimpleBTB(numEntries=128+4096, tagBits=19) # L0 BTB + L1 BTB
-    ras = ReturnAddrStack(numEntries=16)
+    # BTBEntries = 512
+    # BTBTagSize = 19
+    # RASSize = 32
+    btb = SimpleBTB(numEntries=128+6144+12288, tagBits=19)  # L0 BTB + L1 BTB + L2 BTB
+    ras = ReturnAddrStack(numEntries=16) # ?? 
     indirectBranchPred = IndirectPred() # use NULL to disable
 
     tage = LTAGE_BP()
 
 depth = 3
 width = 4
-class SkylakeVerbatimCPU(DerivO3CPU):
+class GoldenCoveVerbatimCPU(DerivO3CPU):
     """ Uncalibrated: Configured based on micro-architecture documentation """
 
     branchPred = BranchPred()
@@ -178,58 +213,11 @@ class SkylakeVerbatimCPU(DerivO3CPU):
     commitWidth = 2*width
     squashWidth = 2*width
 
-    fetchBufferSize = 16
-    fetchQueueSize = 50 # 2 * 25
-    numROBEntries = 224
-    numIQEntries = 97 # 2 * 64
-    LQEntries = 72
-    SQEntries = 56
-    numPhysIntRegs = 180
-    numPhysFloatRegs = 168
-
-depth = 3
-width = 4
-class SkylakeTunedCPU(DerivO3CPU):
-    """ Calibrated: configured to match the performance of hardware """
-
-    branchPred = BranchPred()
-
-    # Pipeline delays
-    fetchToDecodeDelay = depth
-    decodeToRenameDelay = 1
-    renameToIEWDelay = 3*depth
-    issueToExecuteDelay = 1
-    iewToCommitDelay = 2*depth
-
-    forwardComSize = 19
-    backComSize = 19
-
-    # Pipeline widths
-    fetchWidth = width * 2
-    decodeWidth = width * 2
-    renameWidth = 3*width
-    issueWidth = 2*width
-    dispatchWidth = 2*width
-    wbWidth = 2*width
-    commitWidth = 2*width
-    squashWidth = 3*width
-
-    fuPool = ExecUnits()
-    # IntDiv()
-    fuPool.FUList[0].opList[1].opLat = 2
-    # IntMult()
-    fuPool.FUList[1].opList[1].opLat = 2
-    # Load
-    fuPool.FUList[2].count = 12
-    # store
-    fuPool.FUList[4].count = 12
-    fuPool.FUList[6].count = 3
-
-    fetchBufferSize = 16
-    fetchQueueSize = 64
-    numROBEntries = 336
-    numIQEntries = 146
-    LQEntries = 72 * 2
-    SQEntries = 56 * 2
-    numPhysIntRegs = 270
-    numPhysFloatRegs = 252
+    fetchBufferSize = 32
+    fetchQueueSize = 72
+    numROBEntries = 352
+    numIQEntries = 97 # ?
+    LQEntries = 192
+    SQEntries = 114
+    numPhysIntRegs = 280
+    numPhysFloatRegs = 332
---
+++ reverse_llvm_mapping/reverse-resource-mapping.py
@@ -0,0 +1,226 @@
+import argparse
+import re
+
+PATTERN_TD = re.compile(r"defm : (\w+)<(\w+),\s*\[([\w, _]+)\],\s*(\d+),")
+
+PATTERN2_TD = re.compile(r"def : (\w+)<(\w+),\s*\[([\w, _]+)\]>;")
+
+PATTERN_DEF_TD = (
+    r"def : (\w+)<(\w+),\s*\[([\w, _]+)\]>\s*\{[^}]*let\s+Latency\s*=\s*(\d+);\n}"
+)
+
+COMBINED_DEF_TD = r"def\s+(\w+)\s*:\s*ProcResGroup<\[\s*([\w,\s]+)\s*\]>;"
+
+RESOURCE_DEF_TD = r"def\s+(\w+)\s*:\s*ProcResource<(\d+)>;"
+
+IS_PIPELINED: dict[str, bool] = dict({"FloatDiv": False, "FloatSqrt": False, "SimdFloatDiv": False, "SimdFloatSqrt": False, "IntDiv": False})
+
+MANUAL_RESOURCES = dict(
+    {
+        "ADLPPort00_01_05_06_10": [
+            "ADLPPort00",
+            "ADLPPort01",
+            "ADLPPort05",
+            "ADLPPort06",
+            "ADLPPort10",
+        ],
+        "ADLPPort04_09": ["ADLPPort04", "ADLPPort09"],
+        "ADLPPort02_03_07_08_11": [
+            "ADLPPort02",
+            "ADLPPort03",
+            "ADLPPort07",
+            "ADLPPort08",
+            "ADLPPort11",
+        ],
+    }
+)
+
+
+OP_TO_GEM5_OP = dict(
+    {
+        # Float
+        "WriteFCmp": ["FloatCmp"],
+        "WriteFAdd": ["FloatAdd"],
+        "WriteFMul": ["FloatMult"],
+        "WriteCvtSS2I": ["FloatCvt"],
+        "WriteFDiv": ["FloatDiv"],
+        "WriteFSqrt": ["FloatSqrt"],
+        # SIMD Float
+        "WriteFCmpX": ["SimdFloatCmp"],
+        "WriteFAddX": ["SimdFloatAdd", "SimdFloatCmp"],  # MAXPS
+        "WriteFMulX": ["SimdFloatMult"],
+        "WriteCvtPS2I": ["SimdFloatCvt"],
+        "WriteFDivX": ["SimdFloatDiv"],
+        "WriteFSqrtX": ["SimdFloatSqrt"],
+        # Int
+        "WriteALU": ["IntAlu"],
+        "WriteIMul32": ["IntMult"],
+        "WriteDiv32": ["IntDiv"],
+        # SIMD Int
+        "WriteVecALUX": ["SimAdd"],
+        "WriteVecLogicX": ["SimdAlu", "SimdCmp"],
+        "WriteVecShiftX": ["SimdShift"],
+        "WriteVecIMulX": ["SimdMult"],
+        # Load/Store
+        "WriteLoad": ["MemRead", "FloatMemRead"],
+        "WriteStore": ["MemWrite", "FloatMemWrite"],
+        # Misc
+        "WriteBlend": ["SimdMisc"],
+    }
+)
+
+
+class OpDescription:
+    latency: int
+    name: str
+
+    def __init__(self, name: str, latency: int):
+        self.name = name
+        self.latency = latency
+
+    def __str__(self):
+        return f"""OpDesc(opClass="{self.name}", opLat={self.latency}, pipelined={IS_PIPELINED.get(self.name, True)})"""
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return self.name == other.name and self.latency == other.latency
+
+    def __hash__(self):
+        return hash((self.name, self.latency))
+
+
+class PortDescription:
+    name: str
+    op_list: set[OpDescription]
+
+    def __init__(self, name: str, op_list: set[OpDescription]):
+        self.name = name
+        self.op_list = op_list
+
+    def __str__(self):
+        return f"""class {self.name}(FUDesc):
+    opList = [
+        {",\n        ".join([str(op) for op in self.op_list])}
+    ]
+    count = 1"""
+
+    def __repr__(self):
+        return self.__str__()
+
+
+def flatten_list(tl: list) -> list:
+    return [item for sublist in tl for item in sublist]
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Reverse resource mapping")
+    parser.add_argument("filename", type=str, help="Filename for LLVM mapping")
+    args = parser.parse_args()
+
+    # get all lines from the file
+    with open(args.filename, "r") as file:
+        lines = file.readlines()
+
+    with open(args.filename, "r") as file:
+        text = file.read()
+
+    uarch_ports: dict[str, PortDescription] = dict()
+    ops_done: set[str] = set()
+    combined_resources: dict[str, list[int]] = dict()
+
+    # gather all resources
+    match_defs = re.findall(RESOURCE_DEF_TD, text)
+    for match_def in match_defs:
+        name, lat = match_def
+        uarch_ports[name] = PortDescription(name, set())
+
+    # gather all combined resources
+    match_defs = re.findall(COMBINED_DEF_TD, text)
+    for match_def in match_defs:
+        name, op_list = match_def
+        combined_resources[name] = op_list.split(", ")
+
+    # add manual resources
+    for name, op_list in MANUAL_RESOURCES.items():
+        combined_resources[name] = op_list
+
+    def gather_port_oplist(op_list):
+        ports = []
+        for port in op_list.split(", "):
+            if port in combined_resources:
+                ports = ports + combined_resources[port]
+            else:
+                if len(port.split(",")) > 0:
+                    for p in port.split(","):
+                        if p in combined_resources:
+                            ports = ports + combined_resources[p]
+                        else:
+                            ports.append(p)
+                else:
+                    ports.append(port)
+        return ports
+
+    for llvm_name, gem5_names in OP_TO_GEM5_OP.items():
+        for line in lines:
+            match = re.search(PATTERN_TD, line)
+            match2 = re.search(PATTERN2_TD, line)
+            if llvm_name in line and match:
+                op_class = match.group(2)
+                op_list = match.group(3)
+                op_lat = match.group(4)
+
+                if op_class == llvm_name and llvm_name not in ops_done:
+                    ops_done.add(llvm_name)
+                    ports = gather_port_oplist(op_list)
+                    op_descs = [
+                        OpDescription(gem5_name, int(op_lat))
+                        for gem5_name in gem5_names
+                    ]
+                    for port in ports:
+                        for op_desc in op_descs:
+                            uarch_ports[port].op_list.add(op_desc)
+            elif llvm_name in line and match2:
+                op_class = match2.group(2)
+                op_list = match2.group(3)
+                op_lat = 1
+
+                if op_class == llvm_name and llvm_name not in ops_done:
+                    ops_done.add(llvm_name)
+                    ports = gather_port_oplist(op_list)
+                    op_descs = [
+                        OpDescription(gem5_name, int(op_lat))
+                        for gem5_name in gem5_names
+                    ]
+                    for port in ports:
+                        for op_desc in op_descs:
+                            uarch_ports[port].op_list.add(op_desc)
+            elif llvm_name in line:
+                match_defs = re.findall(PATTERN_DEF_TD, text)
+                for match_def in match_defs:
+                    name, op_class, op_list, op_lat = match_def
+                    if op_class == llvm_name and llvm_name not in ops_done:
+                        ops_done.add(llvm_name)
+                        ports = gather_port_oplist(op_list)
+                        op_descs = [
+                            OpDescription(gem5_name, int(op_lat))
+                            for gem5_name in gem5_names
+                        ]
+                        for port in ports:
+                            for op_desc in op_descs:
+                                uarch_ports[port].op_list.add(op_desc)
+
+    # get ordered list of ports
+    uarch_ports = dict(sorted(uarch_ports.items()))
+    # filter out empty ports
+    uarch_ports = {k: v for k, v in uarch_ports.items() if len(v.op_list) > 0}
+    for port in uarch_ports.values():
+        print(port)
+
+    print("class ExecUnits(FUPool):")
+    print("    FUList = [" + ", ".join([f"{port}()" for port in uarch_ports]) + "]")
+
+
+if __name__ == "__main__":
+    main()
